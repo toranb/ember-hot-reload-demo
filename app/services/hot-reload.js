ceec7d68b759abc6ebff2ce8e7cefdc8bd057d0b
toran billups <toranb@gmail.com>
2018-05-09 07:43:04 -0500

    updated hot reload reducers to supply modulePrefix

--- app/services/hot-reload.js
+++ app/services/hot-reload.js
@@ -2,13 +2,14 @@ import { get } from '@ember/object';
 import { combineReducers } from 'redux';
 import Evented from '@ember/object/evented';
 import Service, { inject as service } from '@ember/service';
+import { getOwner } from '@ember/application';
 
-const getReducerModule = function(modulePath) {
+const getReducerModule = function(modulePath, modulePrefix) {
   const fileNamePattern = new RegExp('(.*)/app/reducers/(.*)');
   const match = fileNamePattern.exec(modulePath);
   if (match && match.length === 3) {
     const reducer = match[2].replace('.js', '');
-    return 'todomvc/reducers/' + reducer;
+    return `${modulePrefix}/reducers/${reducer}`;
   }
 };
 
@@ -18,18 +19,20 @@ export default Service.extend(Evented, {
     this._super(...arguments);
     this.on('willLiveReload', this, 'confirmLiveReload');
     this.on('willHotReload', this, 'attemptLiveReload');
+    const factory = getOwner(this).factoryFor('config:environment');
+    this.modulePrefix = factory.class.modulePrefix;
   },
   confirmLiveReload(event) {
-    const module = getReducerModule(event.modulePath);
+    const module = getReducerModule(event.modulePath, this.modulePrefix);
     if (module) {
       event.cancel = true;
       window.requirejs.unsee(module);
     }
   },
   attemptLiveReload(modulePath) {
-    const redux = get(this, 'redux');
-    const module = getReducerModule(modulePath);
+    const module = getReducerModule(modulePath, this.modulePrefix);
     if (module) {
+      const redux = get(this, 'redux');
       const hotReloadedReducer = window.require(module);
       redux.replaceReducer(combineReducers({
         todos: hotReloadedReducer['default']
